e480d9bbc0fbe3c84c31bf95fd809ec90527c13c
Nicolas Gonzalez <nicolasvazquez1@outlook.es>
2022-09-17 14:46:05 -0500

    Dashboard Initial Structure

--- app.py
+++ app.py
@@ -1,105 +1,175 @@
-from dash import dash, html, dcc
-import dash_bootstrap_components as dbc  # pip install dash-bootstrap-components
+from dash import Dash, html, dcc
+import dash_bootstrap_components as dbc
+import numpy as np
 
-from visualizations import chart
+app = Dash(__name__)
 
-# App initialization
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
-# Layout parameters
-app.layout =  html.Div([
-    html.Br(),
 
-    # Heading
-    dbc.Container(children=[
-        dbc.Row([
-            dbc.Col(html.H1(children=["Parametrización macroeconómica"], style={'font-family': 'Arial', 'color': '#D6D4C9'}), width=8, align='left'),
-            dbc.Col(html.Img(src='https://ilab.net/wp-content/uploads/2020/07/citibanamex.png', width='244.8', height='82'))
-        ], justify='left')
-        ]),
+# Create global chart template
+mapbox_access_token = "pk.eyJ1IjoicGxvdGx5bWFwYm94IiwiYSI6ImNrOWJqb2F4djBnMjEzbG50amg0dnJieG4ifQ.Zme1-Uzoi75IaFbieBDl3A"
 
-    # First row level. Param button
-    dbc.Container([
-    dbc.Row([
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                    dcc.Dropdown(id='crisis-choose',
-                             options=['Crisis financiera de 1994',
-                              'Crisis financiera de 2008',
-                              'Crisis financiera por COVID-19'])
-                ])
-            ]),
-        ], width=3),
-        ],className='mb-2'),
+layout = dict(
+    autosize=True,
+    automargin=True,
+    margin=dict(l=30, r=30, b=20, t=40),
+    hovermode="closest",
+    plot_bgcolor="#F9F9F9",
+    paper_bgcolor="#F9F9F9",
+    legend=dict(font=dict(size=10), orientation="h"),
+    title="Satellite Overview",
+    mapbox=dict(
+        accesstoken=mapbox_access_token,
+        style="light",
+        center=dict(lon=-78.05, lat=42.54),
+        zoom=7,
+    ),
+)
 
-    # Second row level
-    dbc.Row([
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                    dcc.Graph(id='inflation-chart', figure={}),
-                ])
-            ]),
-        ], width=4),
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                    dcc.Graph(id='unemploy-chart', figure={}),
-                ])
-            ]),
-        ], width=4),
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                ])
-            ]),
-        ], width=2)
-    ],className='mb-2 mt-4'),
+# Create app layout
+app.layout = html.Div(
+    [
+        dcc.Store(id="aggregate_data"),
+        # empty Div to trigger javascript file for graph resizing
+        html.Div(id="output-clientside"),
+        html.Div(
+            [
+                html.Div(
+                    [
+                        html.Div(
+                            [
+                                html.H1(
+                                    "Herman",
+                                    style={"margin-bottom": "0px", "color": "black", "text-align": "center"},
+                                ),
+                                html.H3(
+                                    "Dashboard Interactivo", style={"margin-top": "0px", "color": "SkyBlue",
+                                                                    "text-align": "center"}
+                                ),
+                            ]
+                        )
+                    ],
+                    className="one-half column",
+                    id="title",
+                ),
+            ],
+            id="header",
+            className="row flex-display",
+            style={"margin-bottom": "25px"},
+        ),
+        html.Div(
+            [
+                html.Div(
+                    [
+                        html.P(
+                            "Filtrar datos por Fecha",
+                            className="control_label",
+                        ),
+                        dcc.RangeSlider(
+                            id="year_slider",
+                            min=0,
+                            max=22,
+                            value=list(np.arange(0, 23, 1)),
+                            className="dcc_control",
+                        ),
+                        html.P("Filtrar por Tipo de Cliente:", className="control_label"),
+                        dcc.RadioItems(
+                            id="well_status_selector",
+                            options=[
+                                {"label": "All ", "value": "all"},
+                                {"label": "Active only ", "value": "active"},
+                                {"label": "Customize ", "value": "custom"},
+                            ],
+                            value="active",
+                            labelStyle={"display": "inline-block"},
+                            className="dcc_control",
+                        ),
+                        dcc.Dropdown(
+                            id="well_statuses",
+                            options=[1, 2, 3],
+                            multi=True,
+                            value=list([1, 2, 3]),
+                            className="dcc_control",
+                        ),
+                        dcc.Checklist(
+                            id="lock_selector",
+                            options=[{"label": "Lock camera", "value": "locked"}],
+                            className="dcc_control",
+                            value=[],
+                        ),
+                    ],
+                    className="pretty_container four columns",
+                    id="cross-filter-options",
+                ),
+                html.Div(
+                    [
+                        html.Div(
+                            [
+                                html.Div(
+                                    [html.H6(id="well_text"), html.P("Top Productos")],
+                                    id="wells",
+                                    className="mini_container",
+                                ),
+                                html.Div(
+                                    [html.H6(id="gasText"), html.P("Producto 1")],
+                                    id="gas",
+                                    className="mini_container",
+                                ),
+                                html.Div(
+                                    [html.H6(id="oilText"), html.P("Producto 2")],
+                                    id="oil",
+                                    className="mini_container",
+                                ),
+                                html.Div(
+                                    [html.H6(id="waterText"), html.P("Producto 3")],
+                                    id="water",
+                                    className="mini_container",
+                                ),
+                            ],
+                            id="info-container",
+                            className="row container-display",
+                        ),
+                        html.Div(
+                            [dcc.Graph(id="count_graph")],
+                            id="countGraphContainer",
+                            className="pretty_container",
+                        ),
+                    ],
+                    id="right-column",
+                    className="eight columns",
+                ),
+            ],
+            className="row flex-display",
+        ),
+        html.Div(
+            [
+                html.Div(
+                    [dcc.Graph(id="main_graph")],
+                    className="pretty_container seven columns",
+                ),
+                html.Div(
+                    [dcc.Graph(id="individual_graph")],
+                    className="pretty_container five columns",
+                ),
+            ],
+            className="row flex-display",
+        ),
+        html.Div(
+            [
+                html.Div(
+                    [dcc.Graph(id="pie_graph")],
+                    className="pretty_container seven columns",
+                ),
+                html.Div(
+                    [dcc.Graph(id="aggregate_graph")],
+                    className="pretty_container five columns",
+                ),
+            ],
+            className="row flex-display",
+        ),
+    ],
+    id="mainContainer",
+    style={"display": "flex", "flex-direction": "column"},
+)
 
-    # Third row level
-    dbc.Row([
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                    dcc.Graph(id='usdmxn-chart', figure={}),
-                ])
-            ]),
-        ], width=8),
-    ],className='mb-2'),
-
-    # Fourth level
-        dbc.Row([
-            dbc.Col([
-                dbc.Card([
-                    dbc.CardBody([
-                        dcc.Graph(id='short-chart', figure={}),
-                    ])
-                ]),
-            ], width=4),
-            dbc.Col([
-                dbc.Card([
-                    dbc.CardBody([
-                        dcc.Graph(id='long-chart', figure={}),
-                    ])
-                ]),
-            ], width=4),
-        ], className='mb-2'),
-
-    # Fifth row level
-    dbc.Row([
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                    dcc.Graph(id='pib-chart', figure={}),
-                ])
-            ]),
-        ], width=10),
-    ], className='mb-2'),
-
-], fluid=True)
-
-    ])
-
-if __name__=='__main__':
-    app.run_server(debug=False, port=8001)
+app.run_server(debug=True)
